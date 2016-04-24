92ddd2771e2ca1b1098983c0a02865cdf974a357
Jan Zaloudek <mail@janzaloudek.cz>
2016-04-24 10:52:47 +0200

    skeleton of lists

--- app/src/main/java/com/cvut/janzaloudek/stm_dia/FieldSelection/FieldAdapter.java
+++ app/src/main/java/com/cvut/janzaloudek/stm_dia/FieldSelection/FieldAdapter.java
@@ -21,21 +21,21 @@ public class FieldAdapter extends ArrayAdapter<Field> {
         super(context, resource, objects);
     }
 
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        // Get the data item for this position
-////        User user = getItem(position);
-//        // Check if an existing view is being reused, otherwise inflate the view
-////        if (convertView == null) {
-////            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_user, parent, false);
-////        }
-////        // Lookup view for data population
-////        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
-////        TextView tvHome = (TextView) convertView.findViewById(R.id.tvHome);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Field field = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_fields_listview_item, parent, false);
+        }
+
+//        // Lookup view for data population
+//        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+//        TextView tvHome = (TextView) convertView.findViewById(R.id.tvHome);
 //        // Populate the data into the template view using the data object
-////        tvName.setText(user.name);
-////        tvHome.setText(user.hometown);
+//        tvName.setText(user.name);
+//        tvHome.setText(user.hometown);
 //        // Return the completed view to render on screen
-//        return convertView;
-//    }
+        return convertView;
+    }
 }
